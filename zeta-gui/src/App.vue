aa41537af6edb61cbc47f0f308defb4819a5e2d7
kmelnikov98 <33918043+kmelnikov98@users.noreply.github.com>
2022-03-20 21:23:06 -0700

    boilerplate
    change name from hello world to something else

--- zeta-gui/src/App.vue
+++ zeta-gui/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <MainPage msg="Zeta Software GUI" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from "./components/MainPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MainPage,
+  },
+};
 </script>
 
 <style>
