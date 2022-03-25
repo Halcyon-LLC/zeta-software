121caf793412ba5ed16dca5adc1473b043161701
kmelnikov98 <33918043+kmelnikov98@users.noreply.github.com>
2022-03-24 23:39:35 -0700

    demo code
    temp code pushed. used for demo

--- zeta-gui/src/App.vue
+++ zeta-gui/src/App.vue
@@ -2,17 +2,38 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <MainPage msg="Zeta Software GUI" />
+    <TextField msg="jesus" @textUpdate="updateFirstName" />
+    <NumberList/>
+    <div> {{ firstName }}</div>
+
   </div>
 </template>
 
 <script>
 import MainPage from "./components/MainPage.vue";
+import TextField from "./components/TextField.vue";
+import NumberList from "./components/NumberList.vue";
 
 export default {
   name: "App",
   components: {
     MainPage,
+    TextField,
+    NumberList,
   },
+
+  data() {
+    return {
+      firstName : "",
+    }
+  },
+
+  methods: {
+    updateFirstName(value) {
+      this.firstName = value;
+    }
+
+  }
 };
 </script>
 
