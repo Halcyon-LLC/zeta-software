4b84d13e264f148cdbad9a64ab55ac4dad2ae37b
Aki Zhou <aki@akizhou.com>
2022-04-09 19:16:09 +0900

    Update firmataClient interface

--- src/firmataClient.js
+++ src/firmataClient.js
@@ -2,13 +2,24 @@ function captureData(payload) {
   return new Promise((resolve, reject) => {
     const { PythonShell } = require('python-shell')
 
-    // TODO: parse payload and pass them to python
+    const path = require('path')
+    const os = require('os')
+
+    let defaultPath = path.join(os.homedir(), 'Downloads')
+    let destination = payload.path == '' ? defaultPath : payload.path
+
+    let now = new Date()
+    now = now.toISOString().slice(0, -5).replaceAll(':', '-')
+
+    let fileName = payload.fileName == '' ? now : payload.fileName
+    fileName += '.csv'
+
     let options = {
       mode: 'text',
       pythonPath: '.venv/bin/python3',
       pythonOptions: ['-u'], // get print results in real-time
       scriptPath: './scripts/',
-      args: ['Tim Cook'],
+      args: ['-d', path.join(destination, fileName)],
     }
 
     PythonShell.run('capture_data.py', options, function (err, results) {
