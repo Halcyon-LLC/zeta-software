f0c15935431e1cb39e9853317ab149da62cd23a8
Aki Zhou <aki@akizhou.com>
2022-04-10 12:17:55 +0900

    Convert analog reading to pressure

--- scripts/capture_data.py
+++ scripts/capture_data.py
@@ -30,7 +30,28 @@ class FSRSampler:
             self.readings[sensor_id] += data
             self.data_size_counter[sensor_id] += 1
 
-    def get_data(self):
+    def _convert_to_pressure(self, readings: np.array) -> np.array:
+        """Convert analog reading to pressure in pascal
+            voltage: voltage readings expressed in mV. float: [0, 5000]
+            mass: mass in kg derived from voltage reading mv
+            force: force in N calculated from m and gravitational acceleration g = 9.80665 m/s^2
+            area: FSR's active area in m^2 which is a circle with diameter of 12.7mm
+
+        Args:
+            readings (np.array): annalog innput readings, ranges float: [0, 1]
+
+        Returns:
+            pressure (np.array): pressure in pascal
+        """
+        voltage = readings * 5000  # mapping range [0, 1] to [0, 5000]
+        mass = 10**((1250 / 809) * np.log10(195300 / (5000 - voltage))) / 1000
+        force = mass * 9.80665
+        area = np.pi * (12.7 / 2)**2 / 1000
+        pressure = force / area
+
+        return pressure
+
+    def get_data(self) -> np.array:
         self.board.samplingOn(1000 / self.sampling_rate)
 
         for id in range(self.num_sensors):
@@ -42,7 +63,7 @@ class FSRSampler:
             print(self.data_size_counter)
             sleep(0.1)
 
-        return self.readings / self.sample_size
+        return self._convert_to_pressure(self.readings / self.sample_size)
 
 
 @click.command()
@@ -51,12 +72,19 @@ class FSRSampler:
 @click.option('--mat-resolution', '-mr', type=int, default=5)
 @click.option('--sample-size', '-n', type=int, default=100)
 def cli(destination, mats, mat_resolution, sample_size):
+    open(destination, 'a').close()
+
     total_num_sensors = mats * mat_resolution
     with FSRSampler(num_sensors=total_num_sensors, sample_size=sample_size) as sampler:
         readings = sampler.get_data()
 
-    open(destination, 'a').close()
-    np.savetxt(destination, readings, fmt='%.5f', delimiter=',', header='pressure')
+    # top left, top right, bottom left, bottom right, center
+    coordinates = np.array([[76.623169, 56.330269, 14.545361], [62.036472, 55.910305, -29.455305],
+                            [66.146713, 5.640682, -32.606579], [88.189301, 6.051844, 16.012398],
+                            [66.687050, 27.577545, -7.667533]])
+
+    out = np.concatenate((coordinates, readings.reshape(total_num_sensors, 1)), axis=1)
+    np.savetxt(destination, out, fmt='%.5f', delimiter=',', header='x,y,z,pressure')
 
 
 if __name__ == '__main__':
