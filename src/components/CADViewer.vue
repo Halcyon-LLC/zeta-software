c0497055ebf1649020e148e05700ba3104a38bd8
kmelnikov98 <33918043+kmelnikov98@users.noreply.github.com>
2022-05-11 20:19:11 -0700

    threejs component
    add threejs component into the code.

---
+++ src/components/CADViewer.vue
@@ -0,0 +1,88 @@
+<template>
+    <div ref="canvas"/>
+</template>
+
+<script>
+import * as THREE from 'three'
+import TrackballControls from 'three-trackballcontrols'
+
+export default {
+    name: 'CADViewer',
+
+    data: function() {
+        const scene = new THREE.Scene()
+        const camera = new THREE.PerspectiveCamera(
+            75,
+            150 / 500,
+            0.1,
+            500
+        )
+
+        const renderer = new THREE.WebGLRenderer({ antialias: true })
+        const light = new THREE.DirectionalLight('hsl(0, 100%, 100%)')
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshStandardMaterial({
+            side: THREE.FrontSide,
+            color: 'hsl(0, 100%, 50%)',
+            wireframe: false
+        })
+        const cube = new THREE.Mesh(geometry, material)
+        const axes = new THREE.AxesHelper(5)
+
+        return {
+            scene: scene,
+            camera: camera,
+            controls: [],
+            renderer: renderer,
+            light: light,
+            cube: cube,
+            axes: axes,
+            speed: 0.01
+        }
+    },
+
+    created() {
+        this.scene.add(this.camera)
+        this.scene.add(this.light)
+        this.scene.add(this.cube)
+        this.scene.add(this.axes)
+        this.renderer.setSize(150, 500)
+        this.light.position.set(0, 0, 5)
+        this.camera.position.z = 5
+        this.scene.background = new THREE.Color('hsl(0, 100%, 100%)')
+        this.controls = new TrackballControls(this.camera)
+        this.controls.rotateSpeed = 1.0
+        this.controls.zoomSpeed = 5
+        this.controls.panSpeed = 0.8
+        this.controls.noZoom = false
+        this.controls.noPan = false
+        this.controls.staticMoving = true
+        this.controls.dynamicDampingFactor = 0.3
+    },
+    mounted() {
+        this.$refs.canvas.appendChild(this.renderer.domElement)
+        this.animate()
+    },
+
+    computed: {
+        rotate() {
+            if (this.speed === '') {
+                return 0
+            } else {
+                return this.speed
+            }
+        }
+    },
+
+    methods: {
+        animate() {
+            requestAnimationFrame(this.animate)
+            this.renderer.render(this.scene, this.camera)
+            this.cube.rotation.y += this.speed
+            this.controls.update()
+        }
+    },
+}
+</script>
+
+<style></style>
