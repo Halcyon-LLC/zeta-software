fbdb8048ee3ca19ae515c08e12b449616374049d
kmelnikov98 <33918043+kmelnikov98@users.noreply.github.com>
2022-05-16 20:54:10 -0700

    CAD viewer
    CAD viewer shows the CAD now

--- src/components/CADViewer.vue
+++ src/components/CADViewer.vue
@@ -18,14 +18,14 @@ export default {
         const scene = new THREE.Scene()
         const camera = new THREE.PerspectiveCamera(
             75,
-            150 / 500, // don't hardcode this
+            window.innerWidth / window.innerHeight, // don't hardcode this 150/ 500
             0.1,
             500
         )
 
         const renderer = new THREE.WebGLRenderer({ antialias: true })
         const light = new THREE.DirectionalLight('hsl(0, 100%, 100%)')
-        const cube = new THREE.Group();
+       // const cube = new THREE.Group();
         // if(this.CADFile) {
         //     const loader = new OBJLoader()
         //     const result = loader.parse( this.CADFile );
@@ -33,12 +33,13 @@ export default {
         //     console.log(result)
         // }
 
-        // const geometry = new THREE.BoxGeometry(1, 1, 1)
-        // const material = new THREE.MeshStandardMaterial({
-        //     side: THREE.FrontSide,
-        //     color: 'hsl(0, 100%, 50%)',
-        //     wireframe: false
-        // })
+        const geometry = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshStandardMaterial({
+            side: THREE.FrontSide,
+            color: 'hsl(0, 100%, 50%)',
+            wireframe: false
+        })
+        const cube = new THREE.Mesh(geometry, material)
         const axes = new THREE.AxesHelper(5)
 
         return {
@@ -58,39 +59,42 @@ export default {
             console.log(this.CADFile )
             const loader = new OBJLoader()
             const result = loader.parse( this.CADFile );
-            this.cube = result //add the 3Dobject to the scene
-            this.scene.add(this.cube)
+           // this.cube = result //add the 3Dobject to the scene
             console.log("Two")
-             console.log(this.scene)
-            this.animate()
+            console.log(this.scene)
+            this.scene.add(result)
+            result.position.set(0, 0, 0)
+            result.rotation.y += this.speed
+          // this.camera.LookAt(result.position)
+            this.createScene()
+            this.startAnimation()
         }
     },
 
-    created() {
-        this.scene.add(this.camera)
-        this.scene.add(this.light)
-       // this.scene.add(this.cube)
-        this.scene.add(this.axes)
-        this.renderer.setSize(150, 500)
-        this.light.position.set(0, 0, 5)
-        this.camera.position.z = 5
-        this.scene.background = new THREE.Color('hsl(0, 100%, 100%)')
+    // created() {
+    //     this.scene.add(this.camera)
+    //     this.scene.add(this.light)
+    //     this.scene.add(this.axes)
+    //     this.renderer.setSize(150, 500)
+    //     this.light.position.set(0, 0, 5)
+    //     this.camera.position.z = 5
+    //     this.scene.background = new THREE.Color('hsl(0, 100%, 100%)')
       
-    },
+    // },
 
-    mounted() {
-        this.$refs.canvas.appendChild(this.renderer.domElement)
-        this.controls = new TrackballControls(this.camera, this.renderer.domElement)
-        this.controls.addEventListener("change", this.render)
-        this.controls.rotateSpeed = 1.0
-        this.controls.zoomSpeed = 5
-        this.controls.panSpeed = 0.8
-        this.controls.noZoom = true
-        this.controls.noPan = false
-        this.controls.staticMoving = true
-        this.controls.dynamicDampingFactor = 0.3
-        this.animate()
-    },
+    // mounted() {
+    //     this.$refs.canvas.appendChild(this.renderer.domElement)
+    //     this.controls = new TrackballControls(this.camera, this.renderer.domElement)
+    //     this.controls.addEventListener("change", this.render)
+    //     this.controls.rotateSpeed = 1.0
+    //     this.controls.zoomSpeed = 5
+    //     this.controls.panSpeed = 0.8
+    //     this.controls.noZoom = true
+    //     this.controls.noPan = false
+    //     this.controls.staticMoving = true
+    //     this.controls.dynamicDampingFactor = 0.3
+    //     this.animate()
+    // },
 
 
     beforeUnmount() {
@@ -107,7 +111,6 @@ export default {
         animate() {
             requestAnimationFrame(this.animate)
             this.renderer.render(this.scene, this.camera)
-            //this.cube.rotation.y += this.speed
             this.controls.update()
         },
 
@@ -118,13 +121,26 @@ export default {
         createScene() {
             this.scene.add(this.camera)
             this.scene.add(this.light)
-            this.scene.add(this.cube)
             this.scene.add(this.axes)
-            this.renderer.setSize(150, 500)
+            this.renderer.setSize(window.innerWidth, window.innerHeight)
             this.light.position.set(0, 0, 5)
             this.camera.position.z = 5
             this.scene.background = new THREE.Color('hsl(0, 100%, 100%)')
-        }
+        },
+
+        startAnimation() {
+            this.$refs.canvas.appendChild(this.renderer.domElement)
+            this.controls = new TrackballControls(this.camera, this.renderer.domElement)
+            this.controls.addEventListener("change", this.render)
+            this.controls.rotateSpeed = 1.0
+            this.controls.zoomSpeed = 5
+            this.controls.panSpeed = 0.8
+            this.controls.noZoom = false
+            this.controls.noPan = false
+            this.controls.staticMoving = true
+            this.controls.dynamicDampingFactor = 0.3
+            this.animate()
+        },
     },
 }
 </script>
